d6b423c7d0dc60a2f5752994378dbeb82c456228
Oleg Kean <oleg.kean@gmail.com>
2013-06-04 21:52:41 +0300

    Updating users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,11 +31,12 @@ describe "AuthenticationPages" do
       
       let(:user) { FactoryGirl.create(:user) }
       before do
-        valid_signin user
+        sign_in user
       end
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
