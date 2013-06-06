54c538bc7c1de3914aa3a68934e47e1b70209e10
Oleg Kean <oleg.kean@gmail.com>
2013-06-06 22:00:56 +0300

    Authorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,4 +46,70 @@ describe "AuthenticationPages" do
       end
     end
   end
+  
+  describe "authorization" do
+    
+    describe "for non-signed-in users" do
+      
+      let(:user) { FactoryGirl.create(:user) }
+      
+      describe "visiting the edit page" do
+        before { visit edit_user_path(user) }
+        it { should have_selector('title', text: 'Sign in') }
+      end
+      
+      describe "submiting the update action" do
+        before { put user_path(user) }
+        specify { response.should redirect_to(signin_path) }
+      end
+      
+      describe "when attempting to visit a protected page" do
+        
+        before do
+          visit edit_user_path(user)
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: user.password
+          click_button 'Sign in'
+        end
+        
+        describe "after signing in" do
+          
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user' )
+          end
+          
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name)
+            end
+          end
+        end
+      end
+    end
+    
+    describe "as a wrong user" do
+      
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@example.com') }
+      before { sign_in user }
+      
+      describe "visiting the User#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+      
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+  end
 end
